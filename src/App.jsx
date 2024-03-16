96deb68c5980892177a1706d213f4d1ba02ad0ab
mim chowdhury <umac3333@gmail.com>
2024-03-16 15:09:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      
+      <h1 className='text-7xl'>hello world</h1>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-outline btn-secondary">Secondary</button>
+
+    </>
+  )
+}
+
+export default App
